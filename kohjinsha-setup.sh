c4bd9ed304f7a355a317ce43ae937e496bb2cddb
daneos <daneos@daneos.com>
2014-05-19 00:56:36 +0200

    suspend, bluetooth, a2dp

---
+++ kohjinsha-setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# This script sets up Crunchbang system to use on Kohjinsha SC3-VHB
+# Please run as root when all drivers are installed
+
+# CHANGE THIS TO YOUR HOME DIRECTORY !!!
+HOME="/home/daneos"
+
+# black screen after resume fix
+echo "ADD_PARAMETERS='-quirk-vbemode-restore'" > /etc/pm/config.d/gma500
+
+# get bluetooth applet working
+apt-get install blueman
+echo "## Start bluetooth applet (blueman)" >> $HOME/.config/openbox/autostart
+echo "(sleep 2s && blueman-applet) &" >> $HOME/.config/openbox/autostart
+
+# enable a2dp
+apt-get install pulseaudio-module-bluetooth
